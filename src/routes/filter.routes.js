5803702763bece2ac56cb6d17a5b735fdf2db3de
RoshanDalami <roshandalami0@gmail.com>
2024-04-07 12:56:49 +0545

    filter with custom date

--- src/routes/filter.routes.js
+++ src/routes/filter.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { validateToken } from "../middlewares/verifyToken.js";
-import {sevenDaysData , fourteenDaysData ,thirtyDaysData,customDateDaysData } from '../controllers/filter.controller.js';
+import {sevenDaysData , fourteenDaysData ,thirtyDaysData,customDateDaysData , filterWithMealTimeAndDate } from '../controllers/filter.controller.js';
 const FilterRoute = express.Router()
 
 
@@ -9,6 +9,7 @@ FilterRoute.route('/sevenDaysData').get(validateToken,sevenDaysData)
 FilterRoute.route('/fourteenDaysData').get(validateToken,fourteenDaysData)
 FilterRoute.route('/thirtyDaysData').get(validateToken,thirtyDaysData)
 FilterRoute.route('/customDateDaysData').get(validateToken,customDateDaysData)
+FilterRoute.route('/customMealTimeAndDate').get(validateToken,filterWithMealTimeAndDate)
 
 
 export {FilterRoute}
