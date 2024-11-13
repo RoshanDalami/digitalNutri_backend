4f82079b1709b41701b306c30a6c27c0a0c73d4f
RoshanDalami <roshandalami0@gmail.com>
2024-11-13 21:15:22 +0545

    added delete account api

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,16 +1,27 @@
 import express from "express";
-import { registerUser, loginUser, currentUser, forgotPassword,updateUserName , verify,sendCode , checkUserWithEmail } from "../controllers/user.controller.js";
-import { validateToken } from "../middlewares/verifyToken.js"
+import {
+  registerUser,
+  loginUser,
+  currentUser,
+  forgotPassword,
+  updateUserName,
+  verify,
+  sendCode,
+  checkUserWithEmail,
+  deleteAccountByUser,
+} from "../controllers/user.controller.js";
+import { validateToken } from "../middlewares/verifyToken.js";
 
 const router = express.Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
-router.route('/').get(validateToken,currentUser);
-router.route('/forgotPassword').post(forgotPassword)
-router.route('/updateUsername').post(validateToken,updateUserName)
-router.route('/verifyemail').post(verify)
-router.route('/sendCode').post(sendCode)
-router.route('/checkUserWithEmail').post(checkUserWithEmail)
+router.route("/").get(validateToken, currentUser);
+router.route("/forgotPassword").post(forgotPassword);
+router.route("/updateUsername").post(validateToken, updateUserName);
+router.route("/verifyemail").post(verify);
+router.route("/sendCode").post(sendCode);
+router.route("/checkUserWithEmail").post(checkUserWithEmail);
+router.route("/deleteAccount").get(validateToken, deleteAccountByUser);
 
 export default router;
